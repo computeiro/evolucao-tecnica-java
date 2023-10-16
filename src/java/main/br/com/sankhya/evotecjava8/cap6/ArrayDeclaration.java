12efdfe51cb15ead3a9a458d0f87d33726667aa8
wender <wender@sankhya.com.br>
2023-10-16 13:59:46 -0300

    Inicializando o projeto

---
+++ src/java/main/br/com/sankhya/evotecjava8/cap6/ArrayDeclaration.java
@@ -0,0 +1,23 @@
+package br.com.sankhya.evotecjava8.cap6;
+
+public class ArrayDeclaration {
+	
+	public static void main(String[] args) {
+		int[] numeros;
+		int numerais[];
+		int [] quantidades;
+		int tamanhos      [] = {1,2,3};
+		
+		
+		numeros = new int[10];
+		
+		//numerais = {1, 2, 3, 4, 6}; //não compila
+		
+		
+		quantidades = new int[-1]; //
+		
+	}
+	
+	
+}
+
